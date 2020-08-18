22cf4b18e6a6d44524569bd1b4e55f090c3a41f1
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2020-08-19 01:32:50 +0530

    finished footer. moving to categories

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,33 @@
+	<div class="footer border-top mt-5 pt-3">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 d-flex align-items-center">
+					<img src="./assets/images/kid-world-logo.png" alt="kid world logo" title="kid world logo" class="img-fluid" />
+				</div>
+				<div class="col-md-4">
+					<h3>Quick Links</h3>
+					<a href="./" class="text-info nav-link h5">Home</a>
+					<a href="/toys" class="text-info nav-link h5">Toys</a>
+					<a href="/cloths" class="text-info nav-link h5">Clothes</a>
+					<a href="/stationaries" class="text-info nav-link h5">Stationaries</a>
+					<a href="/books" class="text-info nav-link h5">Books</a>
+					<a href="/school" class="text-info nav-link h5">School</a>
+				</div>
+			</div>
+		</div>
+		<div class="text-center border-top">
+			<span class="h3">Follow us on</span>
+			<a href="" class="h3">
+				<i class="fa fa-facebook-square ml-3 text-primary" aria-hidden="true"></i>
+			</a>
+			<a href="" class="h3">
+				<i class="fa fa-twitter-square ml-3 text-primary" aria-hidden="true"></i>
+			</a>
+			<a href="" class="h3">
+				<i class="fa fa-instagram ml-3 text-primary" aria-hidden="true"></i>
+			</a>
+		</div>
+	</div>
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
