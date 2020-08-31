0f8c3d0510c19be1d7b79d9567bfd95070c77a18
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2020-08-31 11:02:36 +0530

    finishing login page design

--- app/views/authenticate/login.php
+++ app/views/authenticate/login.php
@@ -1,21 +1,32 @@
-<?php
-@include_once '../app/views/inc/header.php';
-?>
-<div class="login container mt-5">
-	<h1 class="text-dark text-center">LOGIN</h1>
-	<div class="row">
-		<form action="" method="POST">
-			<div class="form-group">
-				<label for="username">Username</label>
-				<input type="text" name="username" />
-			</div>
-			<div class="form-group">
-				<label for="password">Password</label>
-				<input type="password" name="password" />
-			</div>
-			<button class="btn btn-primary">LOGIN</button>
-		</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Kid World</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+	<link rel="stylesheet" href="/Sales&Marketing/public/css/style.css" />
+</head>
+<body>
+	<div class="login container d-flex flex-column align-items-center justify-content-center" style="min-height: 100vh;">
+		<h1 class="text-dark text-center mb-5">LOGIN</h1>
+		<div class="row">
+			<form action="" method="POST" class="text-center">
+				<div class="form-group">
+					<label for="username" class="h5">Username</label>
+					<input type="text" name="username" id="username" />
+				</div>
+				<div class="form-group">
+					<label for="password" class="h5">Password</label>
+					<input type="password" name="password" id="password" />
+				</div>
+				<button class="btn btn-primary float-right">LOGIN</button>
+			</form>
+		</div>
 	</div>
-</div>
-<?php
-@include_once '../app/views/inc/footer.php';
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	<script src="https://use.fontawesome.com/6eee881363.js"></script>
+</body>
+</html>
